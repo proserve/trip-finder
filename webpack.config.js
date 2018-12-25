34af513851d739f8d7e064d43de2c0e12c168788
khalid ghiboub <kghiboub@gmail.com>
2018-12-25 12:28:55 +0400

    Enable analyser only on dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,39 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader', exclude: '/node_modules/' },
-      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      {
-        test: /\.(svg|html|xml)$/i,
-        use: 'raw-loader',
-      }, {
-        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/,
-        use: 'url-loader',
-      },
-    ],
-  },
-  devtool: 'eval-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new CopyWebpackPlugin([{ from: './src/assets/', to: path.resolve(__dirname, 'dist/assets') }]),
-    new BundleAnalyzerPlugin(),
-  ],
+// const isProd = mode === 'production';
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+  }),
+  new CopyWebpackPlugin([{ from: './src/assets/', to: path.resolve(__dirname, 'dist/assets') }]),
+];
+
+module.exports = (env, arg) => {
+  if (arg.mode !== 'production') {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index_bundle.js',
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader', exclude: '/node_modules/' },
+        { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+        {
+          test: /\.(svg|html|xml)$/i,
+          use: 'raw-loader',
+        }, {
+          test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/,
+          use: 'url-loader',
+        },
+      ],
+    },
+    devtool: 'eval-source-map',
+    plugins,
+  };
 };
